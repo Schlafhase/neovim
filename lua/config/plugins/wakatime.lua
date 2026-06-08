4639ff95d23c5ab03d6334bb0520fa3e08a70724
Linus <linus.schneeberg@gmail.com>
2026-06-08 21:59:31 +0200

    only enable wakatime if key is present

--- lua/config/plugins/wakatime.lua
+++ lua/config/plugins/wakatime.lua
@@ -1,5 +1,21 @@
 return {
 	{
 		"vim-wakatime",
+		event = "BufEnter",
+		enabled = function()
+			local cfg = vim.fn.expand("~/.wakatime.cfg")
+			local f = io.open(cfg, "r")
+			if not f then
+				return false
+			end
+			for line in f:lines() do
+				if line:match("^%s*api_key%s*=") or line:match("^%s*api_key_vault_cmd%s*=") then
+					f:close()
+					return true
+				end
+			end
+			f:close()
+			return false
+		end,
 	},
 }
