9af1e0a0e3e04e1ea701e3d0a4b3b4abd5adb9ab
Linus Schneeberg <linus.schneeberg@gmail.com>
2026-08-09 22:50:12 +0200

    fix formatting in change-im file

--- lua/config/custom/change-fcitx5-im.lua
+++ lua/config/custom/change-fcitx5-im.lua
@@ -1,5 +1,20 @@
 local M = {}
 
-local normalIM = "keyboard-gb" local insertIM = "unixkey" M.changeIM = function(name) local success = pcall(vim.fn.system, "fcitx5-remote -s " .. name) if not success then vim.notify("Failed to set fcitx5 input method. Is fcitx5-remote installed?") end end M.useCorrectIM = function() local mode = vim.api.nvim_get_mode().mode if mode == "i" or mode == "c" or mode == "t" then M.changeIM(insertIM) else M.changeIM(normalIM) end end
+local normalIM = "keyboard-gb"
+local insertIM = "unixkey"
+M.changeIM = function(name)
+	local success = pcall(vim.fn.system, "fcitx5-remote -s " .. name)
+	if not success then
+		vim.notify("Failed to set fcitx5 input method. Is fcitx5-remote installed?")
+	end
+end
+M.useCorrectIM = function()
+	local mode = vim.api.nvim_get_mode().mode
+	if mode == "i" or mode == "c" or mode == "t" then
+		M.changeIM(insertIM)
+	else
+		M.changeIM(normalIM)
+	end
+end
 
 return M
